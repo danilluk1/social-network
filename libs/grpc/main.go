6dd5120637bba838136ec64c254eb90a408a7074
danluki <danluki02@yandex.ru>
2023-03-12 16:08:28 +0500

    grpc generator

---
+++ libs/grpc/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	os.RemoveAll("generated")
+
+	files, err := filepath.Glob("./protos/*.proto")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if strings.Contains(file, "google") {
+			continue
+		}
+
+		parts := strings.Split(file, "/")
+		if len(parts) != 2 {
+			log.Fatalf("Folder structure %s doesn't supported", file)
+		}
+		fname := parts[1]
+
+		parts = strings.Split(fname, ".")
+		if len(parts) != 2 {
+			log.Fatalf("File name %s is not expected", file)
+		}
+		name := parts[0]
+
+		if _, err := os.Stat(fmt.Sprintf("generated/%s", file)); os.IsNotExist(err) {
+			os.MkdirAll(fmt.Sprintf("generated/%s", name), os.ModePerm)
+		}
+
+		cmd := exec.Command("protoc",
+			"--go_opt=paths=source_relative",
+			fmt.Sprintf("--go-grpc_out=./generated/%s", name),
+			"--go-grpc_opt=paths=source_relative",
+			"--experimental_allow_proto3_optional",
+			fmt.Sprintf("--go_out=./generated/%s", name),
+			"--proto_path=./protos",
+			file)
+
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			log.Fatal("Error running protoc:", string(output))
+		}
+
+		fmt.Println("✅ Generated", file, "proto definitions", string(output))
+	}
+}
