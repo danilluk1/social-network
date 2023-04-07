ebbfd8322a244ad5074a64869b897012aeecbf2e
danluki <danluki02@yandex.ru>
2023-04-07 17:21:53 +0500

    moving to ent + some tests

--- apps/gateway/internal/api/v1/auth/controller_test.go
+++ apps/gateway/internal/api/v1/auth/controller_test.go
@@ -3,5 +3,15 @@ package auth
 import "testing"
 
 func TestAuthAPI(t *testing.T) {
+	user, _ := randomUser(t)
+
+	testCases := []struct {
+		name          string
+		setupMocks    func()
+		checkResponse func()
+	}
+}
+
+func randomUser(t *testing.T) (*user, error) {
 
 }
